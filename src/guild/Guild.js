137e17756625894c0e8ec2341a5d0d4ace2633e1
Proxxaaa <Spamproxxa@gmail.com>
2021-04-29 15:01:13 -0400

    Guild modification methods

--- src/guild/Guild.js
+++ src/guild/Guild.js
@@ -1,6 +1,9 @@
 const Base = require('../Base')
 const ChannelManager = require('../managers/ChannelManager')
 const fetch = require('node-fetch')
+const VoiceChannel = require('../channel/VoiceChannel')
+const TextChannel = require('../channel/TextChannel')
+const BitField = require('../client/BitField')
 class Guild extends Base {
     /**
      * 
@@ -75,8 +78,14 @@ class Guild extends Base {
              * The guild's channels.
              * @readonly
              */
-            this.channels = new ChannelManager(this.client, guild.channels)
-            
+            Object.defineProperty(this, 'channels', { value: new ChannelManager(this.client, guild.channels) })
+
+            /**
+             * The guild's enabled features.
+             * @readonly
+             */
+            Object.defineProperty(this, 'features', { value: guild.features })
+
         }
         /**
          * The guild id.
@@ -138,6 +147,265 @@ class Guild extends Base {
         return this.edit({ 'name': name })
     }
 
+    /**
+     * Sets the verification level of the guild.
+     * * 0: Unrestricted
+     * * 1: Verified Email
+     * * 2: Registered Discord user for 5+ minutes
+     * * 3: Server member for 10+ minutes
+     * * 4: Verified phone number
+     * @param {string|number} level The level of verification.
+     * @returns {Promise<Guild>} The updated guild.
+     */
+    setVerification(level) {
+        let lev = level
+        if (lev instanceof String) lev = Number(lev)
+        if (isNaN(lev)) {
+            const error = new RangeError("Non-number input. Expected number or stringified number.")
+            error.input = level
+            throw error
+        }
+        if (lev > 4 || lev < 0) throw new RangeError("Expected input to be between zero and four. Was " + lev)
+        if (lev % 1 !== 0) throw new RangeError("Expected input to be an integer. Was " + lev)
+        return this.edit({ 'verification_level': lev })
+    }
+
+    /**
+     * Sets the explicit content filter level of the guild.
+     * * 0: Unfiltered
+     * * 1: Filter content from role-less members
+     * * 2: Filter content from anyone
+     * @param {string|number} level The level of the filter.
+     * @returns {Promise<Guild>} The updated guild.
+     */
+    setFilter(level) {
+        let lev = level
+        if (lev instanceof String) lev = Number(lev)
+        if (isNaN(lev)) {
+            const error = new RangeError("Non-number input. Expected number or stringified number.")
+            error.input = level
+            throw error
+        }
+        if (lev > 2 || lev < 0) throw new RangeError("Expected input to be between zero and two. Was " + lev)
+        if (lev % 1 !== 0) throw new RangeError("Expected input to be an integer. Was " + lev)
+        return this.edit({ 'explicit_content_filter': lev })
+    }
+
+    /**
+     * Sets the afk timeout of this guild's voice channels.
+     * @param {string|number} timeout The new timeout.
+     * @returns {Promise<Guild>} The updated guild.
+     */
+    setTimeout(timeout) {
+        let seconds = timeout
+        if (seconds instanceof String) seconds = Number(seconds)
+        if (isNaN(seconds)) {
+            const error = new RangeError("Non-number input. Expected number or stringified number.")
+            error.input = timeout
+            throw error
+        }
+        if (seconds < 0) throw new RangeError("Expected input to be positive. Was " + seconds)
+        if (seconds % 1 !== 0) throw new RangeError("Expected input to be an integer. Was " + seconds)
+        return this.edit({ 'afk_timeout': seconds })
+    }
+
+    /**
+     * Sets the guild's afk channel.
+     * @param {string|number} channel The id of the new channel.
+     * @returns {Promise<Guild>} The updated guild.
+     */
+    setIdleChannel(channel) {
+        if (!this.channels.cache.has(channel.toString())) {
+            const error = new RangeError("Invalid/uncached channel ID. Try refreshing cache first.")
+            error.id = channel
+            throw error
+        }
+        if (!(this.channels.cache.get(channel.toString()) instanceof VoiceChannel)) {
+            const error = new RangeError("Invalid channel ID. Must be a voice channel.")
+            error.id = channel
+            throw error
+        }
+        return this.edit({ 'afk_channel_id': channel })
+    }
+
+    /**
+     * Sets the default notification level of the guild.
+     * @param {string|number} level The default notification level.
+     * @returns {Promise<Guild>} The updated guild.
+     */
+    setDefaultNotifications(level) {
+        let lev = Number(level)
+        if (lev !== 0 && lev !== 1) throw new RangeError("Expected input to be 0 or 1. Got " + level)
+        return this.edit({ 'default_message_notifications': lev })
+    }
+    
+    /**
+     * Sets the icon of the guild.
+     * @param {Buffer|String} imageData A buffer of image data or Base64 encryption of it.
+     * @returns {Promise<Guild>} The updated guild.
+     */
+    setIcon(imageData) {
+        let dat = imageData
+        if (typeof dat !== 'string') dat = dat.toString('base64')
+        return this.edit({ 'icon': 'data:image/jpeg;base64,' + dat })
+    }
+
+    /**
+     * Sets the splash image of the guild.
+     * @param {Buffer|String} imageData A buffer of image data or Base64 encryption of it.
+     * @returns {Promise<Guild>} The updated guild.
+     */
+    setSplash(imageData) {
+        let dat = imageData
+        if (typeof dat !== 'string') dat = dat.toString('base64')
+        return this.edit({ 'splash': 'data:image/jpeg;base64,' + dat })
+    }
+
+    /**
+     * Sets the discovery splash image of the guild.
+     * @param {Buffer|String} imageData A buffer of image data or Base64 encryption of it.
+     * @returns {Promise<Guild>} The updated guild.
+     */
+    setDiscoverySplash(imageData) {
+        let dat = imageData
+        if (typeof dat !== 'string') dat = dat.toString('base64')
+        return this.edit({ 'discovery_splash': 'data:image/jpeg;base64,' + dat })
+    }
+    
+    /**
+     * Sets the banner image of the guild.
+     * @param {Buffer|String} imageData A buffer of JPEG image data or Base64 encryption of it.
+     * @returns {Promise<Guild>} The updated guild.
+     */
+    setBanner(imageData) {
+        let dat = imageData
+        if (typeof dat !== 'string') dat = dat.toString('base64')
+        return this.edit({ 'banner': 'data:image/jpeg;base64,' + dat })
+    }
+
+    /**
+     * Sets the owner of the guild. Must be the owner of the guild to do this.
+     * @param {String|Number} snowflake The id of the new owner.
+     * @returns {Promise<Guild>} The updated guild.
+     */
+    setOwner(snowflake) {
+        if (this.owner.id !== this.client.user.id) throw Error("Must be the guild owner to transfer ownership.")
+        return this.edit({ 'owner': snowflake })
+    }
+
+    /**
+     * Sets the guild's "system channel."
+     * @param {string|number} channel The id of the new channel.
+     * @returns {Promise<Guild>} The updated guild.
+     */
+    setSystemChannel(channel) {
+        if (!this.channels.cache.has(channel.toString())) {
+            const error = new RangeError("Invalid/uncached channel ID. Try refreshing cache first.")
+            error.id = channel
+            throw error
+        }
+        if (!(this.channels.cache.get(channel.toString()) instanceof TextChannel)) {
+            const error = new RangeError("Invalid channel ID. Must be a text channel.")
+            error.id = channel
+            throw error
+        }
+        return this.edit({ 'system_channel_id': channel })
+    }
+
+    /**
+     * Sets the guild's system channel settings.
+     * @param {BitField|Number} bitfield The flags to set..
+     * @returns {Promise<Guild>} The updated guild.
+     */
+    setSystemFlags(bitfield) {
+        let bits = bitfield
+        if (bits instanceof Number) bits = new BitField(bits)
+        if (bits.bitfield > 7) throw new RangeError("Bitfield cannot have a value greater than 7. Got " + bits.bitfield)
+        return this.edit({ 'afk_channel_id': bits.bitfield })
+    }
+
+    /**
+     * Sets the guild's "rules channel."
+     * @param {string|number} channel The id of the new channel.
+     * @returns {Promise<Guild>} The updated guild.
+     */
+    setRulesChannel(channel) {
+        if (!this.channels.cache.has(channel.toString())) {
+            const error = new RangeError("Invalid/uncached channel ID. Try refreshing cache first.")
+            error.id = channel
+            throw error
+        }
+        if (!(this.channels.cache.get(channel.toString()) instanceof TextChannel)) {
+            const error = new RangeError("Invalid channel ID. Must be a text channel.")
+            error.id = channel
+            throw error
+        }
+        return this.edit({ 'rules_channel_id': channel })
+    }
+
+    /**
+     * Sets the guild's "updates channel."
+     * @param {string|number} channel The id of the new channel.
+     * @returns {Promise<Guild>} The updated guild.
+     */
+    setUpdatesChannel(channel) {
+        if (!this.channels.cache.has(channel.toString())) {
+            const error = new RangeError("Invalid/uncached channel ID. Try refreshing cache first.")
+            error.id = channel
+            throw error
+        }
+        if (!(this.channels.cache.get(channel.toString()) instanceof TextChannel)) {
+            const error = new RangeError("Invalid channel ID. Must be a text channel.")
+            error.id = channel
+            throw error
+        }
+        return this.edit({ 'public_updates_channel_id': channel })
+    }
+
+    /**
+     * Sets the guild's discovery description
+     * @param {string} description The new description.
+     * @returns {Promise<Guild>} The updated guild.
+     */
+    setDescription(description) {
+        description = description.toString()
+        return this.edit({ 'description': description })
+    }
+
+    /**
+     * Sets the guild's features
+     * Valid options are ANIMATED_ICON, BANNER, COMMERCE,
+     * COMMUNITY, DISCOVERABLE, FEATURABLE, INVITE_SPLASH,
+     * MEMBER_VERIFICATION_GATE_ENABLED, NEWS, PARTNERED,
+     * PREVIEW_ENABLED, VANITY_URL, VERIFIED, VIP_REGIONS,
+     * WELCOME_SCREEN_ENABLED
+     * Note that not all of these may be usable in all guilds.
+     * @param {String|String[]} features The new list of features.
+     * @returns {Promise<Guild>} The updated guild.
+     */
+    setFeatures(features) {
+        if (!Array.isArray(features)) features = [features]
+        return this.edit({ 'features': features })
+    }
+
+    /**
+     * Add to the guild's features
+     * Valid options are ANIMATED_ICON, BANNER, COMMERCE,
+     * COMMUNITY, DISCOVERABLE, FEATURABLE, INVITE_SPLASH,
+     * MEMBER_VERIFICATION_GATE_ENABLED, NEWS, PARTNERED,
+     * PREVIEW_ENABLED, VANITY_URL, VERIFIED, VIP_REGIONS,
+     * WELCOME_SCREEN_ENABLED
+     * Note that not all of these may be usable in all guilds or
+     * settable through the API.
+     * @param {String|String[]} features The new list of features.
+     * @returns {Promise<Guild>} The updated guild.
+     */
+    addFeatures(features) {
+        if (!Array.isArray(features)) features = [features]
+        let oldFeats = new Set(this.features)
+        features.forEach(oldFeats.add)
+        return this.edit({ 'features': Array.from(oldFeats) })
+    }
 }
 
 module.exports = Guild
